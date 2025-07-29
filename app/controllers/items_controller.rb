cb6c7d24cecc74ac4f57a551214263b62ee43cbf
kazuki-0707 <kazuki_akariha@yahoo.co.jp>
2025-07-29 14:27:06 +0900

    商品一覧表示機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,8 +2,10 @@
 
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
+    @items = Item.includes(:user).order(created_at: :desc)
   end
 
   def new

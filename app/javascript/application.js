55874289ae65753a5bb09757708d284b9d5a5724
kazuki-0707 <kazuki_akariha@yahoo.co.jp>
2025-08-03 15:53:26 +0900

    インポートマップの修正

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
+import Rails from "@rails/ujs"
+Rails.start()
 import "controllers"
 import "./price_calculation"
 import "./card"

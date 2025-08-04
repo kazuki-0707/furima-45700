a97fd4075e856968402c212dc25a35567c8662d1
kazuki-0707 <kazuki_akariha@yahoo.co.jp>
2025-08-04 11:26:22 +0900

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,6 +29,7 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
   config.assets.digest = true
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
   config.assets.precompile += %w( *.js *.css )
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
